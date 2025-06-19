3a914f3baf50c51bab24716eafa6cb0039588dbc
JMaxRowe <jmaxrowe@gmail.com>
2025-06-19 16:14:57 +0100

    Completed tasks 1-6

--- app.js
+++ app.js
@@ -19,4 +19,63 @@ const game = {
   ],
 }
 
-console.dir(pokemon, { maxArrayLength: null })
+//console.dir(pokemon, { maxArrayLength: null })
+
+console.log(pokemon[58].name)
+
+//console.log(game)
+
+
+/*
+Exercise 3
+1. Add a new property to the `game` object. Let's call it "difficulty".
+2. Choose a value for "difficulty" that you think fits the game. Ex: "Easy", "Med" or "Hard". How would you assign it?
+
+
+Solve Exercise 3 here:
+*/
+
+
+game.difficulty = "Hard";
+
+/*
+Exercise 4
+1. Select a starter Pokémon from the `pokemon` array. Remember, a starter Pokémon's `starter` property is true.
+2. Add this Pokémon to the `game.party` array. Which array method will you use to add them?
+
+
+Solve Exercise 4 here:
+*/
+
+
+game.party.push(pokemon[3])
+
+
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+
+
+game.party.push(pokemon[149]);
+game.party.push(pokemon[129]);
+
+/*
+Exercise 6
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 6 here:
+*/
+
+for(i = 0; i < game.gyms.length ; i++){
+    if (game.gyms[i].difficulty < 3){
+        game.gyms[i].completed = true}
+}
+
